fd1f68737316f3fba6daa3bed06a7afb47c424ef
Ahmed Siddiqui <jesuisahmedn@gmail.com>
2021-02-08 21:48:58 -0800

    Fix function typo

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.set_webook("https://chaddicts-tg-bot.herokuapp.com/" + TOKEN)
+    updater.set_webhook("https://chaddicts-tg-bot.herokuapp.com/" + TOKEN)
 
     # Start the Bot
     updater.start_polling()
