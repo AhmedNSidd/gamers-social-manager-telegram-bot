f3d2b6e6cc7483a31599686cbc1f4f5e63a59586
Ahmed Siddiqui <ahmed@kinsol.io>
2022-06-17 16:54:52 +0300

    Clean up code a bit, and credentials conversation commands

--- bot.py
+++ bot.py
@@ -13,22 +13,18 @@ bot.
 
 import os
 
-from handlers import basic, notify, status, credentials
-from handlers.credentials import TYPING_CLIENT_ID, TYPING_CLIENT_SECRET, TYPING_NPSSO
-from general.production import PRODUCTION_READY
-from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler
-from telegram.ext.filters import Filters
-from db.instantiate_tables import instantiate_tables
+from handlers import basic, notify, status
+from general.production import PRODUCTION_READY, PORT
+from general.values import HEROKU_APP_URL, TOKEN
+from telegram.ext import Updater, CommandHandler
+from scripts.db.instantiate_tables import instantiate_tables
 
-
-PORT = os.environ.get('PORT', 8443)
 
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    TOKEN = os.environ.get("CHADDICTS_TG_BOT_TOKEN")
     updater = Updater(TOKEN, use_context=True)
 
     # Get the dispatcher to register handlers
@@ -55,30 +51,6 @@ def main():
     dp.add_handler(CommandHandler("del_playstation_status_user", status.del_playstation_status_user))
     dp.add_handler(CommandHandler("list_playstation_status_user", status.list_playstation_status_users))
     dp.add_handler(CommandHandler("playstation_status", status.playstation_status))
-    dp.add_handler(ConversationHandler(
-        entry_points=[CommandHandler("xbox_credentials_setup", credentials.xbox_credentials_setup)],
-        states={
-            TYPING_CLIENT_ID: [
-                MessageHandler(Filters.text & (~Filters.command), credentials.store_xbox_client_id)
-            ],
-            TYPING_CLIENT_SECRET: [
-                MessageHandler(Filters.text & (~Filters.command), credentials.store_xbox_client_secret)
-            ]
-        },
-        fallbacks=[MessageHandler(Filters.command, credentials.cancel)],
-    ))
-    dp.add_handler(ConversationHandler(
-        entry_points=[CommandHandler("playstation_credentials_setup", credentials.playstation_credentials_setup)],
-        states={
-            TYPING_NPSSO: [
-                MessageHandler(Filters.text & (~Filters.command), credentials.store_playstation_npsso)
-            ]
-        },
-        fallbacks=[MessageHandler(Filters.command, credentials.cancel)],
-    ))
-
-    # on noncommand i.e message - echo the message on Telegram
-    # dp.add_handler(MessageHandler(Filters.text, echo))
 
     # log all errors
     dp.add_error_handler(basic.error)
@@ -91,7 +63,7 @@ def main():
         updater.start_webhook(listen="0.0.0.0",
                             port=PORT,
                             url_path=TOKEN)
-        updater.bot.set_webhook("https://chaddicts-tg-bot.herokuapp.com/" + TOKEN)
+        updater.bot.set_webhook(f"{HEROKU_APP_URL}/{TOKEN}")
     else:
         updater.start_polling()
 
