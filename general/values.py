15bb56af97621d46aa022ccf80de2739f87d7b40
Ahmed Siddiqui <ahmed@kinsol.io>
2022-06-20 01:49:43 +0300

    Refract token name in values.py from chaddicts to gsm

--- general/values.py
+++ general/values.py
@@ -45,6 +45,6 @@ ADMIN_LIST = [247340182]
 
 BOT_URL = "https://t.me/GamersSocialManagerBot"
 HEROKU_APP_URL = os.environ.get("APP_URL")
-TOKEN = os.environ.get("CHADDICTS_TG_BOT_TOKEN")
+TOKEN = os.environ.get("GSM_TG_BOT_TOKEN")
 XBOX_CLIENT_SECRET_EXPIRY_DATE = datetime.datetime.strptime(os.environ.get("XBOX_CLIENT_SECRET_EXPIRY_DATE", "Jun 16, 2024"), "%b %d, %Y").date()
 
