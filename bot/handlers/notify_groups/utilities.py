fa9ca0c3bd99b7f1ac2b77aa1838735781be81ff
Ahmed Siddiqui <ahmed@kinsol.io>
2022-08-26 09:33:35 +0500

    Addresses #31
    This commit adds the ability to add optional notify messages for
    /notify. This was done by the following acts:
    
    - Fix UI text mistake if no notify group is provided
    - Use context.args instead of message entities to extract name and
    message
    - Add formatting to the notify message

--- bot/handlers/notify_groups/utilities.py
+++ bot/handlers/notify_groups/utilities.py
@@ -5,7 +5,6 @@ from handlers.common import get_one_mention, send_loud_and_silent_message
 from handlers.notify_groups.common import stringify_notify_group
 
 
-# TODO (Issue #32): Allow entry of an optional message when using /notify
 def notify(update, context):
     """
     This method grabs the name of the notify group provided by the user and
@@ -13,6 +12,7 @@ def notify(update, context):
     """
     user_id = update.message.from_user.id
     chat_id = update.message.chat.id
+    curr_user_mention = get_one_mention(context.bot, user_id, chat_id)
 
     # Send an error if the command is called not in a group chat.
     if chat_id == user_id:
@@ -22,18 +22,20 @@ def notify(update, context):
         )
         return
 
-    notify_group_name = update.message.text[
-        update.message.entities[0].length:
-    ].strip()
     # Send an error message if no notify group name was provided.
-    if not notify_group_name:
+    if not context.args:
         update.message.reply_text(
-            "You need to provide a notify group name to join e\.g\. `/notify "
-            "group_name`",
+            "You need to provide a notify group name to notify e\.g\. "
+            "`/notify group_name`",
             parse_mode=ParseMode.MARKDOWN_V2,
             quote=True
         )
         return
+
+    # Get the notify group name and optional message (if it exists)
+    notify_group_name = context.args[0]
+    notify_group_message = (" ".join(context.args[1:])
+                            if len(context.args) > 1 else None)
 
     # Get corresponding notify group from db
     notify_group = DBConnection().find_one(
@@ -57,9 +59,6 @@ def notify(update, context):
         notify_group_creator_mention = get_one_mention(
             context.bot, notify_group['creator_id'], chat_id
         )
-        curr_user_mention = get_one_mention(
-            context.bot, user_id, chat_id
-        )
         send_loud_and_silent_message(
             context.bot,
             "Processing\.\.\.",
@@ -77,8 +76,17 @@ def notify(update, context):
     # Create the notify message, tagging all the notify group members
     notify_msg = ""
     for member_id in notify_group["members"]:
+        if user_id == member_id:
+            continue
         user_mention = get_one_mention(context.bot, member_id, chat_id)
         notify_msg += f"{user_mention} "
+
+    if notify_group_message:
+        notify_msg += (f"\n\n{curr_user_mention} is saying "
+                       f"`{notify_group_message}`")
+    else:
+        notify_msg += f"\n\n{curr_user_mention} wants your attention"
+
 
     # Send the notify message, tagging all the notify group members
     update.message.reply_text(
