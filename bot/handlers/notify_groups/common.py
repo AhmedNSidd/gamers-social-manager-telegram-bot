aca77f8534e7ee5f2b3bfa4d2a64e0cd90f4860d
Ahmed Siddiqui <ahmed@kinsol.io>
2022-08-26 20:18:50 +0500

    Addresses #40 and #49
    This commit achieves three 3 things:
    - Fixes a bug in get_many_mentions func where usernames weren't
    being escaped for markdown
    - Adds 'Left the group' tag for notify group creators and members
    - Fixes bug where invited users aren't being displayed

--- bot/handlers/notify_groups/common.py
+++ bot/handlers/notify_groups/common.py
@@ -1,5 +1,6 @@
-from telegram import Bot
+from telegram import Bot, ChatMember
 from telegram.utils.helpers import escape_markdown
+from general.db import DBConnection
 from handlers.common import get_one_mention
 
 
@@ -8,42 +9,70 @@ def stringify_notify_group(bot: Bot, notify_group: dict):
     Given a dictionary that contains fields of a notify group, this function
     will return a formatted string message that displays this notify group.
     """
-    # Get the creator label
+    # Set up all the string vars for the notify group info
+    notify_group_name = escape_markdown(notify_group["name"], 2)
     creator_mention = get_one_mention(
         bot, notify_group['creator_id'], notify_group['chat_id']
     )
-    notify_group_name = escape_markdown(notify_group["name"], 2)
+    creator_status = bot.get_chat_member(notify_group["chat_id"],
+                                         notify_group['creator_id']).status
+    if creator_status in [ChatMember.KICKED, ChatMember.LEFT]:
+        creator_mention += f" *\[Left the group\]*"
 
     notify_group_description = (escape_markdown(notify_group["description"], 2)
                                 if notify_group["description"] else "None")
 
-    # Add group name and group description
-    s = (
+    # Add current members
+    if notify_group["members"]:
+        left_members_mentions = []
+        members_str = ""
+        for member_id in notify_group["members"]:
+            member_status = bot.get_chat_member(notify_group["chat_id"],
+                                                member_id).status
+            user_mention = get_one_mention(bot, member_id,
+                                           notify_group['chat_id'])
+            if member_status in [ChatMember.KICKED, ChatMember.LEFT]:
+                # Store a list of the mentions of members who are left so we
+                # can add them at the end
+                left_members_mentions.append(f"{user_mention} *\(Left the group\)*\n")
+            else:
+                members_str += f"{user_mention}\n"
+
+        # Append the members who have left at the end of the members list
+        for mention in left_members_mentions:
+            members_str += f"{mention}\n"
+    else:
+        members_str = "`None`"
+
+
+    # Add current invited users
+    notify_group_invitations = DBConnection().find(
+        "notifygroupinvitation", {"notify_group_id": notify_group["_id"]}
+    )
+
+    if notify_group_invitations:
+        invited_str = ""
+        all_actively_invited_mentions = set([])
+        for invitation in notify_group_invitations:
+            for invitee_identifier in invitation["actively_invited"]:
+                if type(invitee_identifier) == str:
+                    all_actively_invited_mentions.add(invitee_identifier)
+                else:
+                    all_actively_invited_mentions.add(get_one_mention(
+                        bot, invitee_identifier, notify_group["chat_id"]
+                    ))
+
+        for mention in all_actively_invited_mentions:
+            invited_str += f"{mention}\n"
+    else:
+        invited_str = "`None`"
+
+    return (
         f"*{notify_group_name}* _\(Created by {creator_mention}\)_\n"
         "__Group Description__\n"
         f"`{notify_group_description}`\n"
         "__Current Members__\n"
+        f"{members_str}"
+        "__Invited Users__\n"
+        f"{invited_str}"
     )
-    # Add current members
-    if notify_group["members"]:
-        for member_id in notify_group["members"]:
-            if member_id == notify_group["creator_id"]:
-                s += f"{creator_mention}\n"
-                continue
-            # TODO (#40): Check for error here, the member could have left the
-            # chat group.
-            s += f"{get_one_mention(bot, member_id, notify_group['chat_id'])}\n"
-    else:
-        s += "`None`"
-
-    # Add current invited users
-    s += "__Invited Users__\n"
-    if notify_group["invited"]:
-        for invited_user_identifier in notify_group["invited"]:
-            if type(invited_user_identifier) == str:
-                s += f"{invited_user_identifier}\n"
-            else:
-                s += f"{get_one_mention(bot, invited_user_identifier, notify_group['chat_id'])}\n"
-    else:
-        s += "`None`"
-    return s
